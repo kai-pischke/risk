02a0b393b99573966e076a9c5fb12bfc6b80e160
pookibrine <68516560+pookibrine@users.noreply.github.com>
2020-07-23 16:55:55 +0100

    Update Battle.hs
    Defined Enum on Defenders and Attackers

--- src/Battle.hs
+++ src/Battle.hs
@@ -5,11 +5,24 @@ module Battle
     ) where
 
 import System.Random
+data Defenders = OneDef | TwoDef deriving (Eq, Show, Ord)
+data Attackers = OneAtt | TwoAtt | ThreeAtt deriving (Eq, Show, Ord)
 
-data Defenders = OneDef | TwoDef 
-               deriving (Eq, Show, Ord, Enum) -- note: deriving Enum won't index correctly
-data Attackers = OneAtt | TwoAtt | ThreeAtt
-               deriving (Eq, Show, Ord, Enum)
+instance Enum Defenders where
+    fromEnum OneDef = 1
+    fromEnum TwoDef = 2 
+    toEnum 1 = OneDef
+    toEnum 2 = TwoDef
+    toEnum n = error ("toEnum{Defenders}: tag (" ++ show n ++ ") is outside of enumeration's range (1,2)")
+
+instance Enum Attackers where
+    fromEnum OneAtt = 1
+    fromEnum TwoAtt = 2
+    fromEnum ThreeAtt = 3 
+    toEnum 1 = OneAtt
+    toEnum 2 = TwoAtt
+    toEnum 3 = ThreeAtt
+    toEnum n = error ("toEnum{Attackers}: tag (" ++ show n ++ ") is outside of enumeration's range (1,3)")
 
 doBattle :: Attackers -> Defenders -> StdGen -> (Int, Int, StdGen)
 doBattle = undefined
