c0848abdba5aa1d7a0b9d4ea324dff2f39a9080a
AlexP-ay <alexdavidpay@outlook.com>
2020-07-29 11:51:57 +0100

    Updated Moves.hs

--- src/Moves.hs
+++ src/Moves.hs
@@ -8,24 +8,74 @@ module Moves (
     skipFortify
 ) where
 
+
+  --imports
   import Battle
   import State
   import RiskBoard
 
+  currPlayer :: GameState -> Player
+  currPlayer = head.turnOrder
+
+  --change later to  calculate continent bonuses
+  nReinforcements :: GameState ->Player -> Int
+  nReinforcements _ _ = 5
+
   reinforce :: [(Country, Int)] -> GameState -> Maybe GameState
-  reinforce = undefined
+  reinforce movs gs | phase gs == Reinforce && validMovList movs = Just $ nextPhase $ foldr ((.).miniChange) id movs gs
+                    | otherwise = Nothing
+    where miniChange (c, nTroops) = changeTroops c nTroops
+          validMovList = valid (nReinforcements gs (currPlayer gs))
+          valid 0 []  = True
+          valid _ []  = False
+          valid n ((c,t) : ms) = (owner gs c == currPlayer gs)
+                                && (t > 0)
+                                && valid (n-t) ms
 
-  attack :: Attackers -> Defenders -> Country -> Country -> GameState -> Maybe GameState
-  attack = undefined
+  attack :: Country -> Country -> Attackers -> Defenders -> GameState -> Maybe GameState
+  attack cAtt cDef att def gs | valid = (Just . makeChange (doBattle att def $ currentStdGen gs)) gs
+                              | otherwise = Nothing
+    where
+      valid = (troops gs cAtt > fromEnum att) &&
+              (troops gs cDef >= fromEnum def) &&
+              (owner gs cAtt == currPlayer gs) &&
+              (owner gs cDef /= currPlayer gs) &&
+              (cDef `isNeighbour` cAtt) &&
+              (phase gs == Attack Normal)
+      makeChange (attLosses, defLosses, stdGen) =
+        if (defLosses == troops gs cDef) --attacker has won a country
+          then (changeOwner cDef (currPlayer gs)) .
+            (changeMiniPhase (WonBattle cAtt cDef (toEnum (nInvaders)))) .
+            regularChanges
+          else regularChanges
+        where regularChanges = (changeTroops cAtt (-attLosses)) . (changeTroops cDef (-defLosses)) . (updateStdGen stdGen)
+              nInvaders = fromEnum att - attLosses
 
+  -- Must invade with at least the number of attackers left
   invade :: Int -> GameState -> Maybe GameState
-  invade = undefined
+  invade nTroops gs = f (phase gs)
+    where
+      f (Attack (WonBattle cAtt cDef attLeft))
+        | (nTroops >= fromEnum attLeft) && (nTroops < troops gs cAtt) = Just $ changeTroops cDef nTroops gs
+        | otherwise = Nothing
+      f _ = Nothing
 
   endAttack :: GameState -> Maybe GameState
-  endAttack = undefined
+  endAttack gs = f (phase gs)
+    where f (Attack Normal) = Just (nextPhase gs)
+          f _ = Nothing
 
   fortify :: Country -> Country -> Int -> GameState -> Maybe GameState
-  fortify = undefined
+  fortify cFrom cTo nTroops gs
+    | valid = (Just . nextPhase . (changeTroops cFrom (-nTroops)) . (changeTroops cTo nTroops)) gs
+    | otherwise = Nothing
+    where valid = (phase gs == Fortify) &&
+                  (nTroops < troops gs cFrom) &&
+                  (nTroops > 0) &&
+                  (owner gs cFrom == currPlayer gs) &&
+                  (owner gs cTo == currPlayer gs) &&
+                  (cTo `isNeighbour` cFrom)
 
   skipFortify :: GameState -> Maybe GameState
-  skipFortify = undefined
+  skipFortify gs | phase gs == Fortify = Just (nextTurn gs)
+                 | otherwise = Nothing
