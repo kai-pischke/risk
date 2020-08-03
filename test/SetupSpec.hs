4a225773e972393879bd858431c5d5883195da64
Pooki <pooki@pop-os.localdomain>
2020-08-04 00:15:18 +0100

    Added SetupSpec - Not sure if this test works can't test due to lack of setupBoard

--- test/SetupSpec.hs
+++ test/SetupSpec.hs
@@ -16,7 +16,7 @@ addOneAllCountry s = foldr (\f x -> (fromJust.f) x) s fs
         fs = map placeTroop [toEnum 0 ..]
 
 completeBoard:: Int -> [Country] -> SetupState -> SetupState
-completeBoard 0 s = s
+completeBoard 0 _ s = s
 completeBoard n (c:cs) s = (completeBoard (n-1) (cs++c). placeTroop c) s
 
 spec :: Spec
@@ -43,9 +43,25 @@ spec = do
         context "2 Player Board" $ do
             let completed2pBoard = completeBoard 80 setupGame
             let b2p = newGame [Red, Black] (completeBoardOwner completedBoard) (mkStdGet 0)
+
             it "2 player game has correct owners" $ do
                 (all (\x -> owner b (fromEnum x) == Red) (filter even [0..41])) `shouldBe` True
                 (all (\x -> owner b (fromEnum x) == Black) (filter odd [0..41])) `shouldBe` True
 
             it "2 player game has correct number troops" $ do
                 (all (\x -> troops b (fromEnum x) == 2) [0..37]) `shouldBe` True
+                (all (\x -> troops b (fromEnum x) == 1) [38..41]) `shouldBe` True
+
+        context "3 Player Game" $ do
+            let completed4pBoard = completeBoard 105 (emptyBoard [Red, Black, Blue])
+
+            let b3p = newGame [Red, Black, Blue] (completeBoardOwner completedBoard) (mkStdGet 0)
+
+            it "3 player game has correct owners" $ do
+                (all (\x -> owner b (fromEnum x) == Red) (filter (\x -> mod x 3 == 0) [0..41])) `shouldBe` True
+                (all (\x -> owner b (fromEnum x) == Black) (filter (\x -> mod x 3 == 1) [0..41])) `shouldBe` True
+                (all (\x -> owner b (fromEnum x) == Blue) (filter (\x -> mod x 3 == 2) [0..41])) `shouldBe` True
+
+            it "3 player game has correct number troops" $ do
+                (all (\x -> troops b (fromEnum x) == 3) [0..20]) `shouldBe` True
+                (all (\x -> troops b (fromEnum x) == 2) [21..41]) `shouldBe` True
