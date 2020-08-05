09aaffa6d17178978bd1b90a210fecfe1dd11e8f
Pooki <pooki@pop-os.localdomain>
2020-08-05 10:39:16 +0100

    Fixed Indentation

--- src/SetupBoard.hs
+++ src/SetupBoard.hs
@@ -1,4 +1,4 @@
-module SetupBoard 
+module SetupBoard
         ( SetupState(..),
           emptyBoard,
           placeTroop,
@@ -19,9 +19,9 @@ data SetupBoardState = InternalGameState
                     statePlayers :: [Player],
                     playerRemaining ::  Map Player Int
                 } deriving (Eq, Show)
-                
-data SetupState = Incomplete SetupBoardState 
-                                                                | PartiallyComplete SetupBoardState 
+
+data SetupState = Incomplete SetupBoardState
+                                                                | PartiallyComplete SetupBoardState
                                                                 | Complete SetupBoardState
                                                                 deriving (Eq, Show)
 -- map functions --
@@ -40,17 +40,17 @@ changeRemaining :: (Map Player Int -> Map Player Int) -> SbStateTransformer
 changeRemaining f (InternalGameState tm pm sp pr) = InternalGameState tm pm sp (f pr)
 
 -- helper functions --
-currentPlayer :: SetupBoardState -> Player 
-currentPlayer = head . statePlayers 
+currentPlayer :: SetupBoardState -> Player
+currentPlayer = head . statePlayers
 
 toSetupState :: SetupBoardState -> SetupState
 toSetupState s | not $ null $ Map.filter isNothing $ playerMap s    = Incomplete s
                              | not $ null $ Map.filter (/= 0) $ playerRemaining s = PartiallyComplete s
-                             | otherwise 
+                             | otherwise
                                                                                                          = Complete s
 nextTurn :: SetupBoardState -> SetupBoardState
 nextTurn = changePlayers rotate
-        where 
+        where
                 rotate [] = []
                 rotate (x:xs) = xs ++ [x]
 
@@ -60,54 +60,55 @@ distinct (x:xs) = (not $ x `elem` xs) && distinct xs
 
 -- public --
 emptyBoard :: [Player] -> SetupState
-emptyBoard ps = Incomplete $ InternalGameState 
-                                                        (Map.fromList $ zip countries $ repeat 0)
-                                                        (Map.fromList $ zip countries $ repeat Nothing)
-                                                        ps
-                                                        (Map.fromList $ zip ps $ repeat $ initialTroops $ length ps)
-                                                where 
-                                                        countries = [(minBound :: Country)..]
-                                                        
-                                                        
-                                             
-placeTroop :: Country -> SetupState -> Maybe SetupState
-placeTroop c (Incomplete s) = case Map.lookup c (playerMap s) of 
-                                                                Just Nothing -> if valid then Just $ toSetupState $ newBoard else Nothing
-                                                                _            -> Nothing
-                                                        where 
-                                                                p = currentPlayer s 
-                                                                valid = maybe (error $ "Player '" ++ show p ++ "' not playing.") 
-                                                                                            (>0) (Map.lookup p $ playerRemaining s)
-                                                                ownerAdded = changePlayerMap (Map.insert c (Just p)) s
-                                                                decremented = changeRemaining (Map.insertWith (flip (-)) p 1) ownerAdded
-                                                                newBoard = nextTurn $ decremented
+emptyBoard ps = Incomplete $ InternalGameState
+    (Map.fromList $ zip countries $ repeat 0)
+    (Map.fromList $ zip countries $ repeat Nothing)
+    ps
+    (Map.fromList $ zip ps $ repeat $ initialTroops $ length ps)
+    where
+        countries = [(minBound :: Country)..]
 
-                                                                
-placeTroop c (PartiallyComplete s) = case Map.lookup c (playerMap s) of 
-                                                                         (Just (Just x)) -> if valid && x == p 
-                                                                                                                then Just $ toSetupState $ newBoard 
-                                                                                                                else Nothing
-                                                                         _               -> error "impossible situation: Nothing in PartiallyComplete"
-                                                        where 
-                                                                p = currentPlayer s 
-                                                                valid = maybe (error $ "Player '" ++ show p ++ "' not playing.") 
-                                                                                            (>0) (Map.lookup p $ playerRemaining s)
-                                                                decremented = changeRemaining (Map.insertWith (flip (-)) p 1) s
-                                                                newBoard = nextTurn $ decremented
+
+
+placeTroop :: Country -> SetupState -> Maybe SetupState
+placeTroop c (Incomplete s) = case Map.lookup c (playerMap s) of
+    Just Nothing -> if valid then Just $ toSetupState $ newBoard else Nothing
+    _            -> Nothing
+    where
+        p = currentPlayer s
+        valid = maybe (error $ "Player '" ++ show p ++ "' not playing.")
+                                    (>0) (Map.lookup p $ playerRemaining s)
+        ownerAdded = changePlayerMap (Map.insert c (Just p)) s
+        decremented = changeRemaining (Map.insertWith (flip (-)) p 1) ownerAdded
+        newBoard = nextTurn $ decremented
+
+
+placeTroop c (PartiallyComplete s) = case Map.lookup c (playerMap s) of
+     (Just (Just x)) -> if valid && x == p
+                                            then Just $ toSetupState $ newBoard
+                                            else Nothing
+     _               -> error "impossible situation: Nothing in PartiallyComplete"
+    where
+        p = currentPlayer s
+        valid = maybe (error $ "Player '" ++ show p ++ "' not playing.")
+                                    (>0) (Map.lookup p $ playerRemaining s)
+        decremented = changeRemaining (Map.insertWith (flip (-)) p 1) s
+        newBoard = nextTurn $ decremented
 
 placeTroop _ _ = error "invalid call to placeTroop: called on 'Complete' SetupState."
 
 completeBoardOwner :: SetupState -> Country -> (Player, Int)
 completeBoardOwner (Complete s) c = result
-        where 
+        where
                 Just result = (,) <$> (join $ Map.lookup c $ playerMap s) <*> (Map.lookup c $ troopMap s)
 completeBoardOwner _ _ = error "completeBoardOwner only defined for Complete SetupBoardState"
--- private -- 
+
+-- private --
 initialTroops :: Int -> Int
 initialTroops 6 = 20
 initialTroops 5 = 25
 initialTroops 4 = 30
 initialTroops 3 = 35
 initialTroops 2 = 40
-initialTroops n = error $ "Game rules only specified for 2-6 players. Not sure what to do for " 
+initialTroops n = error $ "Game rules only specified for 2-6 players. Not sure what to do for "
                                                                                                 ++ show n ++ " players."
