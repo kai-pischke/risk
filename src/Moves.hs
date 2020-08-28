a6c2f28ba55261d327c8967c632ae500473b2f3c
AlexP-ay <alexdavidpay@outlook.com>
2020-08-28 17:30:55 +0100

    implemented trade function

--- src/Moves.hs
+++ src/Moves.hs
@@ -22,6 +22,12 @@ module Moves (
   import State
   import RiskBoard
   import GameElements
+
+  import System.Random
+  import Data.Array.ST
+  import Control.Monad
+  import Control.Monad.ST
+  import Data.STRef
   ---------------------------------------------
 
   ---- Helper Functions ------------------------
@@ -141,15 +147,37 @@ module Moves (
       f (Attack (WonBattle cAtt cDef attLeft))
         | not (cAtt `isNeighbour` cDef) || (owner gs cAtt == owner gs cDef) || (owner gs cAtt /= currPlayer gs) = error "Impossible MiniPhase"
         | (nTroops >= fromEnum attLeft) && (nTroops < troops gs cAtt) =
-          Just $ ((changeMiniPhase Normal) .forceDiscard . (tryKick (owner gs cAtt) (owner gs cDef)) . (drawCard (currPlayer gs) shuffle) . (changeOwner cDef (currPlayer gs)). (changeTroops cAtt (-nTroops)) . (changeTroops cDef nTroops)) gs
+          Just $ ((changeMiniPhase Normal) .forceDiscard . (tryKick (owner gs cAtt) (owner gs cDef)) . (drawCard (currPlayer gs)) . (changeOwner cDef (currPlayer gs)). (changeTroops cAtt (-nTroops)) . (changeTroops cDef nTroops)) gs
         | otherwise = Nothing
-        where shuffle = undefined --Make shuffle a thing
-              forceDiscard = undefined --TODO make force discard a thing
+        where forceDiscard gs' = if (length (cards gs' (currPlayer gs')) >= 6)
+                                   then changeMiniPhase TimeToTrade gs'
+                                   else gs'
       f _ = Nothing
       tryKick :: Player -> Player -> GameState -> GameState
       tryKick pAtt pDef = if all ((/= pDef).owner gs) [toEnum 0 :: Country ..]
                         then kick pAtt pDef
                         else id
+
+  trade :: TradeIn -> [(Country, Int)] -> GameState -> Maybe GameState
+  trade tr movs gs | validTrade tr && phase gs == Attack TimeToTrade && validMovList movs =
+    Just $ nextPhase $ (useCards tr) $ foldr ((.).(uncurry changeTroops)) id movs gs
+                    | otherwise = Nothing
+    where validMovList = valid (tradeInBonus tr)
+          valid 0 []  = True
+          valid _ []  = False
+          valid n ((c,t) : ms) = (owner gs c == currPlayer gs)
+                                && (t > 0)
+                                && valid (n-t) ms
+          validTrade None = True
+          validTrade (OneSet s) = t s && length (cards gs (currPlayer gs)) - 3 < 5
+          validTrade (TwoSet s1 s2) = t s1 && t s2 && length (cards gs (currPlayer gs)) - 3 > 4 && length (cards gs (currPlayer gs)) - 6 < 5
+          t (Wild, _, _) = True
+          t (_, Wild, _) = True
+          t (_, _, Wild) = True
+          t (x, y, z) = (x == y && y == z) || (x /= y && x /= z && y /= z)
+          useCards None = id
+          useCards (OneSet (x, y, z)) = (useCard (currPlayer gs) x) . (useCard (currPlayer gs) y) . (useCard (currPlayer gs) z)
+          useCards (TwoSet s1 s2) = (useCards (OneSet s1)) . (useCards (OneSet s2))
 
   endAttack :: GameState -> Maybe GameState
   endAttack gs = f (phase gs)
